bff41d0f2b70f0741258f444899eb1e12b13e2db
Sangati Shah <74624195+sangati-shah@users.noreply.github.com>
2025-10-29 19:13:07 -0400

    fixing first page

--- scripts/main.js
+++ scripts/main.js
@@ -23,3 +23,9 @@
 //   requestAnimationFrame(updateGradient);
 // }
 // requestAnimationFrame(updateGradient);
+const dateDisplayElement = document.getElementById("currentDateDisplay");
+const currentDate = new Date();
+const options = { weekday: "short", day: "numeric", month: "short", year: "numeric" };
+const formattedDate = currentDate
+  .toLocaleDateString("en-GB", options)
+dateDisplayElement.innerHTML = formattedDate;
